3ebb6a665e15a7016a8a9f8c17e4034b36a5f594
VicMakau-78 <kyamavictor78@gmail.com>
2026-03-14 12:45:23 +0300

    react

--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+
+  // define the two hooks for capturing/storing the users input
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Declare the three additional hooks
+  const [loading, setLoading] = useState("");
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  // Below we have the useNavigate hookto redirect us to another page on successful login.
+  const navigate = useNavigate("");
+
+  // Below is the function to handle the sign in action
+  const handlesubmit = async(e) => {
+    // Prevent the site from reloading
+    e.preventDefault()
+
+    // update the loading hook with a message
+    setLoading("Please wait as we authenticate your account")
+
+    try{
+      // Create a formData object that will hold the emial and the password
+      const formdata = new FormData()
+
+      // Insert/append the email and password on the formdata created
+      formdata.append("email", email);
+      formdata.append("password", password);
+
+      // interact with axios for the response
+      const response = await axios.post("https://vicmakau.alwaysdata.net/api/signin", formdata);
+
+      // set loading hook back to default
+      setLoading("");
+
+      // check whether the user exists as part of your response from the API
+      if(response.data.user){
+        // if user is there, definitely the details entered during signin are correct
+        // setSuccess('Log in Successful')
+        // If it is successful, let the user get redirected to another page
+        navigate ("/");
+      }
+      else{
+        // user is not found, that means the credential enterd on the form are incorrect
+        setError("Log in Failed. Please Try Again...")
+      }
+    }
+
+    catch(error){
+      // set loading back to default
+      setLoading("")
+
+      // update the error hook with  message
+      setError("Oops, Something went wrong. Try again...")
+    }
+  }
+
   return (
-    <div>
-        <h1>Welcome to Signin page</h1>
+    <div className='row justify-content-center mt-4'>
+      <div className="col-md-6 card shadow p-4">
+        <h1 className='text-primary'>Sign in</h1>
+        <h5 className="text-info"> {loading} </h5>
+        <h3 className="text-success"> {success} </h3>
+        <h4 className="text-danger"> {error} </h4>
+
+        <form onSubmit={handlesubmit}>
+          <input type="email"
+          placeholder='Enter Email address..'
+          className='form-control'
+          required 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/> <br />
+
+          {/* {email} */}
+
+          <input type="password"
+          placeholder='Enter password'
+          className='form-control'
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/> <br />
+
+          {/* {password} <br /> */}
+
+          <input type="submit" 
+          value="Sign in"
+          className='btn btn-primary' />
+        </form>
+
+      </div>
     </div>
   )
 }
 
 export default Signin
+
+// how can you store the users details on the local storage
